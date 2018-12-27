5d4ca55cdee40d5548d3aaa63ec565dae50f1e15
Dominic Neu <dneu-dilsburg@gmx.de>
2018-12-27 19:57:19 +0100

    Tensorflow Model Klasse erstellt.

--- DoomPlayer/DoomPlayer/DoomPlayer.py
+++ DoomPlayer/DoomPlayer/DoomPlayer.py
@@ -126,3 +126,122 @@ training = True
 episode_render = False
 
 
+class DQNetwork:
+    def __init__(self, state_size, action_size, learning_rate, name='DQNetwork'):
+        self.state_size = state_size
+        self.action_size = action_size
+        self. learning_rate = learning_rate
+
+        with tf.variable_scope(name):
+            # Platzhalter erstellen
+            self.inputs_ = tf.placeholder(tf.float32, [None, *state_size], name="inputs")
+            self.actions_ = tf.placeholder(tf.float32, [None, 3], name="actions")
+
+            self.target_Q = tf.placeholder(tf.float32, [None], name="target")
+
+            """ 1. Convolutional Layer"""
+            # Input 84x84x4
+            self.conv1 = tf.layers.conv2d(
+                inputs_= self.inputs_,
+                filters = 32,
+                kernel_size = [8, 8],
+                strides = [4, 4], 
+                padding = "VALID",
+                kernel_initializer = tf.contrib.layers.xavier_initializer_conv2d(),
+                name = "conv1"
+                )
+
+            self.conv1_batchnorm = tf.layers.batch_normalization(
+                self.conv1,
+                training = True,
+                epsilon = 1e-5,
+                name = "batch_norm1"
+                )
+
+            self.conv1_out = tf.nn.elu(
+                self.conv1_batchnorm, 
+                name = "conv1_out"
+                )
+            # --> [20, 20, 32]
+
+            """2. Convolutional Layer"""
+            self.conv2 = tf.layers.conv2d(
+                inputs_= self.conv1_out,
+                filters = 64,
+                kernel_size = [4, 4],
+                strides = [2, 2], 
+                padding = "VALID",
+                kernel_initializer = tf.contrib.layers.xavier_initializer_conv2d(),
+                name = "conv2"
+                )
+
+            self.conv2_batchnorm = tf.layers.batch_normalization(
+                self.conv2,
+                training = True,
+                epsilon = 1e-5,
+                name = "batch_norm2"
+                )
+
+            self.conv2_out = tf.nn.elu(
+                self.conv2_batchnorm, 
+                name = "conv2_out"
+                )
+            # --> [9, 9, 64]
+
+            """3. Convolutional Layer"""
+            self.conv3 = tf.layers.conv2d(
+                inputs_= self.conv3_out,
+                filters = 128,
+                kernel_size = [4, 4],
+                strides = [2, 2], 
+                padding = "VALID",
+                kernel_initializer = tf.contrib.layers.xavier_initializer_conv2d(),
+                name = "conv3"
+                )
+
+            self.conv3_batchnorm = tf.layers.batch_normalization(
+                self.conv3,
+                training = True,
+                epsilon = 1e-5,
+                name = "batch_norm3"
+                )
+
+            self.conv3_out = tf.nn.elu(
+                self.conv3_batchnorm, 
+                name = "conv3_out"
+                )
+            # --> [3, 3, 128]
+
+            """Flatten Layer"""
+
+            self.flatten = tf.layers.flatten(
+                self.conv3_out
+                )
+            # --> [1152]
+
+            """Fully Connected Layer"""
+            self.fc = tf.layers.dense(
+                inputs = self.flatten,
+                units = 512,
+                activation = tf.nn.elu,
+                kernel_initializer = tf.contrib.layers.xavier_initializer(),
+                name="fc1"
+                )
+
+            self.output = tf.layers.dense(
+                inputs = self.fc,
+                kernel_initializer = tf.contrib.layers.xavier_initializer(),
+                units = 3,
+                activation = None
+                )
+
+            # Q ist der verhergesagte Q Wert
+
+            self.Q = tf.reduce_sum(tf.multiply(self.output, self.actions_), axis = 1)
+
+            # Loss zwischen vorhergesagtem Q und Q Target
+
+            self.loss = tf.reduce_mean(tf.square(self.target_Q - self.Q))
+
+            self.optimizer = tf.train.RMSPropOptimizer(self.learning_rate).minimize(self.loss)
+
